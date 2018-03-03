e55b70e9a31ba6dea1ca14de9b4d49ee07874820
ICANDIGITAL <Christopher.Leader@Bison.Howard.Edu>
2018-03-03 10:07:11 -0500

    Revert "Chapter 8 Completion"
    This reverts commit bf28a183e859f248bddaa3a1c3d89b83cffd2b6c.

--- chapter_6/favorite_places.py
+++
@@ -1,7 +0,0 @@
-favorite_places = {
-    'chris' : 'rio',
-    'jonathan' : 'california',
-    'kadar' : 'atlanta',
-}
-for name, vacations in favorite_places.items():
-    print(name.title() + ' would like to go to ' + vacations.title() + " soon.")
